36c54f71bc613a89dff0af72bd6e10d3d165c93d
JasonFruit <jasonfruit@hushmail.com>
2014-09-24 23:19:06 -0500

    Beginnings of work on tab-drawing.

--- tab.py
+++ tab.py
@@ -1,8 +1,8 @@
 from fractions import Fraction
 from collections import namedtuple
 
-class Meters(object):
-    whole = 1
+class Durations(object):
+    whole = Fraction(1, 1)
     dot_half = Fraction(3,4)
     half = Fraction(1, 2)
     dot_quarter = Fraction(3, 8)
@@ -14,37 +14,55 @@ class Meters(object):
     dot_thirtysecond = Fraction(3, 64)
     thirtysecond = Fraction(1, 32)
 
-tab = {"meter": (4,4),
-       "partial": 0,
-       "strings": ("e,", "a,", "d", "g", "b", "e'"),
-       "title": "An Example",
-       "composer": "Jason R. Fruit",
-       "content":
-       [(Meters.quarter, [("0", 0),
-                          ("1", 1),
-                          ("0", 2),
-                          ("3", 4)]),
-        (Meters.quarter, [("0", 0),
-                          ("1", 1),
-                          ("0", 2),
-                          ("3", 4)]),
-        (Meters.half, [("0", 1),
-                       ("1", 1),
-                       ("0", 2),
-                       ("3", 4)]),
-        (Meters.quarter, [("0", 0),
-                          ("1", 1),
-                          ("0", 2),
-                          ("3", 4)]),
-        (Meters.quarter, [("0", 0),
-                          ("1", 1),
-                          ("0", 2),
-                          ("3", 4)]),
-        (Meters.half, [("0", 1),
-                       ("1", 1),
-                       ("0", 2),
-                       ("3", 4)])]}
+meter = namedtuple("meter", "numerator denominator duration")
 
-class LayoutEngine(object):
+meters = {"6/4": meter(6, 4, Durations.whole + Durations.half),
+          "4/4": meter(4, 4, Durations.whole),
+          "3/4": meter(3, 4, Durations.dot_half),
+          "2/4": meter(2, 4, Durations.half),
+          "12/8": meter(12, 8, Durations.dot_quarter * 4),
+          "9/8": meter(9, 8, Durations.dot_half + Durations.dot_quarter),
+          "6/8": meter(6, 8, Durations.dot_half),
+          "3/8": meter(3, 8, Durations.dot_quarter),
+          "2/8": meter(2, 8, Durations.quarter)}
+
+class TabDrawer(object):
     def __init__(self, tab):
         self.tab = tab
+        self.measure_duration = meters[tab["meter"]].duration
+
+if __name__ == "__main__":
+
+    tab = {"meter": "4/4",
+           "partial": 0,
+           "strings": ("e,", "a,", "d", "g", "b", "e'"),
+           "title": "An Example",
+           "composer": "Jason R. Fruit",
+           "content":
+           [(Durations.quarter, [("0", 0),
+                                 ("1", 1),
+                                 ("0", 2),
+                                 ("3", 4)]),
+            (Durations.quarter, [("0", 0),
+                                 ("1", 1),
+                                 ("0", 2),
+                                 ("3", 4)]),
+            (Durations.half, [("0", 1),
+                              ("1", 1),
+                              ("0", 2),
+                              ("3", 4)]),
+            (Durations.quarter, [("0", 0),
+                                 ("1", 1),
+                                 ("0", 2),
+                                 ("3", 4)]),
+            (Durations.quarter, [("0", 0),
+                                 ("1", 1),
+                                 ("0", 2),
+                                 ("3", 4)]),
+            (Durations.half, [("0", 1),
+                              ("1", 1),
+                              ("0", 2),
+                              ("3", 4)])]}
+    td = TabDrawer(tab)
+    print str(td.measure_duration / Durations.quarter) + " quarter notes per measure"
+    print dir(Durations.quarter)
